0475048f6616cc63f4edc22e72abcf5193d14f28
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-02-08 16:44:29 +0900

    예제 도메인 모델과 동작확인

--- src/main/java/study/querydsl/Controller/HelloController.java
+++ src/main/java/study/querydsl/Controller/HelloController.java
@@ -1,4 +1,14 @@
 package study.querydsl.Controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
 }
