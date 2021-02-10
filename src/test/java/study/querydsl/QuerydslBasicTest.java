3df7360abf0d91166f80f704c2f3784dc095aa00
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-02-10 23:33:14 +0900

    SQL function 호출

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
 import study.querydsl.dto.QMemberDto;
@@ -595,6 +596,88 @@ public class QuerydslBasicTest {
     private  BooleanExpression allEq(String usernameCond,Integer ageCond){
         return usernameEq(usernameCond).and(ageEq(ageCond));
     }
+
+    @Test
+    public void bulkUpdate(){
+        //member1 = 10 ->비회원
+        //member2 = 20 ->비회원
+        //member3 = 30 ->유지
+        //member4 = 40 ->유지
+
+        long count = queryFactory
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28))
+                .execute();
+
+        em.flush();
+        em.clear();
+
+        List<Member> result=queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        for(Member member1 : result){
+            System.out.println("member1 = "+member1);
+        }
+
+    }
+
+    /**
+     * 기존 숫자에 더하기
+     */
+
+    @Test
+    public void bulkMul(){
+        queryFactory
+                .update(member)
+                .set(member.age,member.age.multiply(2))
+                .execute();
+    }
+
+    /**
+     * 삭제
+     */
+
+    @Test
+    public void bulkDelete(){
+        long count = queryFactory
+                .delete(member)
+                .where(member.age.gt(18))
+                .execute();
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    public void sqlFunction(){
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate("function('replace',{0},{1},{2})",
+                        member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        for(String s:result){
+            System.out.println("s = "+s);
+        }
+    }
+    @Test
+    public void sqlFunction2(){
+        List<String> result=queryFactory
+                .select(member.username)
+                .from(member)
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for(String s:result){
+            System.out.println("s = "+s);
+        }
+    }
+
+
+
 
 
 
