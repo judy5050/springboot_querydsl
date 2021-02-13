5b0935a95a0a37a1df681608fa1781d9a9001617
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-02-13 23:18:43 +0900

    수정

--- src/main/java/study/querydsl/Controller/MemberController.java
+++ src/main/java/study/querydsl/Controller/MemberController.java
@@ -1,8 +1,11 @@
 package study.querydsl.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.querydsl.Controller.repository.MemberRepository;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.Controller.repository.MemberJpaRepository;
@@ -14,10 +17,16 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition){
         return memberJpaRepository.search(condition);
 
+    }
+
+    @GetMapping("/v2/members2")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchPageSimple(condition,pageable);
     }
 }
